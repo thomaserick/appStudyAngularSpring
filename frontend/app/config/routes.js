b6e7e6ef6233e66fa9a82cb7a8874085735af6bf
Thomas <thominhaserick@gmail.com>
2020-11-16 22:27:41 -0300

    Configurando Rotas with Ui Router

---
+++ frontend/app/config/routes.js
@@ -0,0 +1,16 @@
+angular.module('App').config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+        $stateProvider
+        .state('dashboard',{
+            url:'/dashboard',
+            templateUrl:'dashboard/dashboard.html'
+        }).state('billingCycle', {
+            url:'/billingCycles',
+            templateUrl:"billingCycle/tabs.html"
+        })
+
+        $urlRouterProvider.otherwise('/dashboard');
+    }
+])

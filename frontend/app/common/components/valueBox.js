3ba35c351602fd036df8e800c4f0ac1ce01b097f
Thomas <thominhaserick@gmail.com>
2020-11-16 23:52:38 -0300

    Components ValueBox

---
+++ frontend/app/common/components/valueBox.js
@@ -0,0 +1,17 @@
+angular.module('App').component('valueBox', {
+    bindings: {
+        grid:'@',
+        colorClass:'@',
+        value:'@',
+        text:'@',
+        iconClass:'@',
+
+    },
+    controller:[
+        "gridSystem",
+        function (gridSystem) {
+          this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid);
+        },
+    ],
+    templateUrl: 'common/components/valueBoxComponent.html'
+})

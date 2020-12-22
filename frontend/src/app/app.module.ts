4eca858a6d8118de21997d6c9572b11608a872a7
Tef <thominhaserick@gmail.com>
2020-12-21 23:31:56 -0300

    Modules customers

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CustomersModule } from './customers/customers.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomersModule
   ],
   providers: [],
   bootstrap: [AppComponent]

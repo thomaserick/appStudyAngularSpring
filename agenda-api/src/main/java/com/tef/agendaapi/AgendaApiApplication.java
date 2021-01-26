ddc4ac96057cb2565694910d1a0dd4c05148709b
Tef <thominhaserick@gmail.com>
2021-01-25 22:39:08 -0300

    Teste insert contact BD

--- agenda-api/src/main/java/com/tef/agendaapi/AgendaApiApplication.java
+++ agenda-api/src/main/java/com/tef/agendaapi/AgendaApiApplication.java
@@ -1,10 +1,27 @@
 package com.tef.agendaapi;
 
+import com.tef.agendaapi.Repository.ContactRepository;
+import com.tef.agendaapi.entity.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AgendaApiApplication {
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner(@Autowired ContactRepository contactRepository){
+//
+//        return args -> {
+//            Contact contact = new Contact("Thomas","thominhas",true);
+//            contactRepository.save(contact);
+//
+//        };
+//
+//    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(AgendaApiApplication.class, args);

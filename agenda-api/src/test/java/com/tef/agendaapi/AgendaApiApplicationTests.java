6b5b1d41156040b6cdac8eae47116b894c307770
Tef <thominhaserick@gmail.com>
2021-01-25 22:10:36 -0300

    start project back-end

---
+++ agenda-api/src/test/java/com/tef/agendaapi/AgendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tef.agendaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
